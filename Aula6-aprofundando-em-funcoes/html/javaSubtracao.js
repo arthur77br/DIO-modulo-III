ab11c0b5fe160f99235170712772951b2f6b574d
arthur77br <bolivarartur77@gmail.com>
2022-06-03 22:03:49 -0300

    Adicionado mais conteudo de javascript

---
+++ Aula6-aprofundando-em-funcoes/html/javaSubtracao.js
@@ -0,0 +1,13 @@
+
+
+
+const btn = document.querySelector("#send");
+btn.addEventListener("click", function(e){
+    e.preventDefault();
+    const name  = document.querySelector("#name");
+    const name1  = document.querySelector("#name1");
+    var n1  = name.value;
+    var n2  = name1.value;
+    resultado = parseInt(n1) - parseInt(n2);
+   document.getElementById("resultado").innerHTML = "Resultado: "+resultado;
+});
